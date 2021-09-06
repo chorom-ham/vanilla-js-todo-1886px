ba367cac0d2a80b97684ea4f2bb3dfdc5aa4e366
usernamebuzz <78242552+usernamebuzz@users.noreply.github.com>
2021-09-06 20:55:40 +0900

    make list

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+var list = document.getElementById("list");
 
+document.getElementById("submit").onclick = function makelist() {
+  var li = document.createElement("li");
+  li.innerText = document.getElementById("input").value;
+  list.appendChild(li);
+  li.id = "li";
+
+  var deleteButton = document.createElement("button");
+  deleteButton.innerText = "x";
+  deleteButton.id = "deleteButton";
+  li.appendChild(deleteButton);
+};
