0f62701d9b08b6547a38c8fa1e12858ba923e0b9
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-07 17:19:06 +0900

    addList 함수 생성
    할 일 목록 추가 기능 구현

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+const main = document.querySelector("main");
 const form = document.querySelector("form");
 const newList = form.querySelector("input");
 
+const addList = () => {
+  const div = document.createElement("div");
+  const input = document.createElement("input");
+  const label = document.createElement("label");
+  const remove = document.createElement("span");
+  const value = newList.value;
+
+  input.type = "checkbox";
+  input.id = "todo";
+  label.htmlFor = "todo";
+  label.innerText = value;
+  remove.innerText = "삭제";
+  div.append(input);
+  div.append(label);
+  div.append(remove);
+  main.append(div);
+  newList.value = "";
+
+  remove.addEventListener("click", handleDelete);
+};
+
 const handleSubmit = (event) => {
   event.preventDefault();
+  addList();
 };
 
 form.addEventListener("submit", handleSubmit);
