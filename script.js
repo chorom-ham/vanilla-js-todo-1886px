e2871ffaa9756701704890ec3209f12612b1c1a1
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-07 16:44:51 +0900

    Create submit handler

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const form = document.querySelector("form");
+const newList = form.querySelector("input");
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+};
+
+form.addEventListener("submit", handleSubmit);
