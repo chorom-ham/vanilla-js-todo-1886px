236f6054a0d9ab226d8fb966bea5926272292734
songing01 <79829662+songing01@users.noreply.github.com>
2021-09-08 17:45:07 +0900

    removeItem

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var button = document.getElementById('button');
 var input = document.getElementById('input');
 var list = document.getElementById('list');
+var cnt=1;
 
 function addItem(){
   var temp = document.createElement('li');
+  temp.setAttribute("id", "li"+cnt);
   temp.innerHTML = input.value;
+  temp.innerHTML += "<button type='button' onclick='deleteItem("+cnt+")'>삭제</button>";
   list.appendChild(temp);
+ 
+}
+function deleteItem(){
+  var li= documet.getElementById('li'+cnt);
+  list.removeChild(li);
 }
