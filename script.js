1bbe8c08352e80810799423c5e528f6feaee594f
songing01 <79829662+songing01@users.noreply.github.com>
2021-09-08 17:02:25 +0900

    addItem

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+var button = document.getElementById('button');
+var input = document.getElementById('input');
+var list = document.getElementById('list');
 
+function addItem(){
+  var temp = document.createElement('li');
+  temp.innerHTML = input.value;
+  list.appendChild(temp);
+}
