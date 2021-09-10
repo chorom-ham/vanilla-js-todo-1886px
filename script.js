360bc73a45905b6efd32dba59837ebbaf1aa3150
Yeonhee_Jung <spig0126@gmail.com>
2021-09-11 00:24:48 +0900

    create js file

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+function addToList(){
+    var listContainer = document.getElementById('listSection');
+    var newListElement = document.createElement("div");
+    var inputBox= document.getElementById('addText');
+    var inputBoxText = inputBox.value;
 
+    newListElement.setAttribute("class", "listElement");
+    newListElement.innerHTML="\
+        <img src='./pinkCircleIcon.png'>\
+        <button class='textButton' name='undone' type='button' value='' onclick='doneOrUndone(this);'></button>\
+        <button class='removeButton' type='button' onclick='removeFromList(this);'></button>";
+    newListElement.children[1].textContent= inputBoxText;
+    listContainer.appendChild(newListElement);
+    inputBox.value = '할 일을 입력하세요';
+}
+
+function removeFromList(ob){
+    var parent = ob.parentElement;
+    var listSection = document.getElementById('listSection');
+    listSection.removeChild(parent);
+}
+
+function doneOrUndone(ob){
+    var parent = ob.parentElement;
+
+    if(ob.name == 'done'){
+        ob.style.fontStyle="normal";
+        ob.style.textDecoration="none";
+        ob.name = 'undone';
+        parent.style.backgroundColor="rgb(7,26,82)";
+    }
+    else{
+        ob.style.fontStyle="italic";
+        ob.style.textDecoration="line-through";
+        ob.name = 'done';
+        parent.style.backgroundColor="rgb(7,26,82, 0.5)";
+    }
+}
