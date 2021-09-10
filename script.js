821c6ed513bce7191d329441ea8d43fb1ac3b51b
spiritstone <mjk4534@gmail.com>
2021-09-11 04:24:04 +0900

    삭제 버튼

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const todoButton = document.getElementById("todo_button");
 const todoInput = document.getElementById("todo_input");
 const todoList = document.getElementById("todo_list");
-const deleteButton = document.getElementById("delete_button");
+//const deleteButton = document.getElementById("delete_button");
+var cnt = 1;
 
-todoButton.addEventListener("click", function () {
+todoButton.addEventListener("click", clickButton);
+
+function clickButton() {
   const newTodo = document.createElement("li");
-  newTodo.innerText = todoInput.value;
+  newTodo.setAttribute("id", "li" + cnt);
+  newTodo.innerHTML = todoInput.value;
+  newTodo.innerHTML +=
+    "<button style='float: right:' class='todo_button' type='button' onclick='remove(" +
+    cnt +
+    ")'> X </button>";
   todoList.appendChild(newTodo);
+  cnt++;
   todoInput.value = "";
   newTodo.addEventListener("click", function () {
     newTodo.style.textDecoration = "line-through";
   });
-});
+}
+function remove(cnt) {
+  var li = document.getElementById("li" + cnt);
+  todoList.removeChild(li);
+}
 
-deleteButton.addEventListener("click", function () {
-  todoList.removeChild(list);
-});
+//deleteButton.addEventListener("click", removeFromList(this));
