450de8d8c5ebbf8f0efe12b3a7653430c4ce17ca
spiritstone <mjk4534@gmail.com>
2021-09-11 03:35:21 +0900

    add todo list and strike out finished task

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const todoButton = document.getElementById("todo_button");
+const todoInput = document.getElementById("todo_input");
+const todoList = document.getElementById("todo_list");
+const deleteButton = document.getElementById("delete_button");
 
+todoButton.addEventListener("click", function () {
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  todoList.appendChild(newTodo);
+  todoInput.value = "";
+  newTodo.addEventListener("click", function () {
+    newTodo.style.textDecoration = "line-through";
+  });
+});
+
+deleteButton.addEventListener("click", function () {
+  todoList.removeChild(list);
+});
