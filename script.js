72d7b423e1e53518e0f9c0a15c24a3cbeb03e79d
usernamebuzz <ohminji@ewhain.net>
2021-09-11 21:33:22 +0900

    delete+toggle

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ document.getElementById("submit").onclick = function makelist() {
   var li = document.createElement("li");
   li.innerText = document.getElementById("input").value;
   list.appendChild(li);
+  li.id = "li";
 
   var deleteButton = document.createElement("button");
-  deleteButton.innerText = "X";
+  deleteButton.innerText = "x";
   deleteButton.id = "deleteButton";
   li.appendChild(deleteButton);
-  deleteButton.addEventListener("click", function () {
+
+  li.addEventListener("click", function () {
     li.classList.toggle("done");
   });
+  
+  deleteButton.addEventListener("click", function (){
+    list.removeChild(li);
+  });
 };
-
 
